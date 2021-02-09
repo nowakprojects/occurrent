f14d1975cd9aa6952b9a3c54398db1493a076ec5
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-02-09 15:31:59 +0100

    #71 EventStore SQL - JDBC | SpringJdbcEventStore start implementation

---
+++ eventstore/sql/spring/blocking/src/main/java/org/occurrent/eventstore/sql/spring/blocking/SpringJdbcEventStore.java
@@ -0,0 +1,72 @@
+package org.occurrent.eventstore.sql.spring.blocking;
+
+import io.cloudevents.CloudEvent;
+import org.occurrent.eventstore.api.WriteCondition;
+import org.occurrent.eventstore.api.blocking.EventStore;
+import org.occurrent.eventstore.api.blocking.EventStoreOperations;
+import org.occurrent.eventstore.api.blocking.EventStoreQueries;
+import org.occurrent.eventstore.api.blocking.EventStream;
+import org.occurrent.filter.Filter;
+
+import java.net.URI;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+class SpringJdbcEventStore implements EventStore, EventStoreOperations, EventStoreQueries {
+
+  @Override
+  public void write(String streamId, WriteCondition writeCondition, Stream<CloudEvent> events) {
+
+  }
+
+  @Override
+  public void deleteEventStream(String streamId) {
+
+  }
+
+  @Override
+  public void deleteEvent(String cloudEventId, URI cloudEventSource) {
+
+  }
+
+  @Override
+  public void delete(Filter filter) {
+
+  }
+
+  @Override
+  public Optional<CloudEvent> updateEvent(String cloudEventId, URI cloudEventSource, Function<CloudEvent, CloudEvent> updateFunction) {
+    return Optional.empty();
+  }
+
+  @Override
+  public Stream<CloudEvent> query(Filter filter, int skip, int limit, SortBy sortBy) {
+    return null;
+  }
+
+  @Override
+  public long count(Filter filter) {
+    return 0;
+  }
+
+  @Override
+  public boolean exists(Filter filter) {
+    return false;
+  }
+
+  @Override
+  public boolean exists(String streamId) {
+    return false;
+  }
+
+  @Override
+  public EventStream<CloudEvent> read(String streamId, int skip, int limit) {
+    return null;
+  }
+
+  @Override
+  public void write(String streamId, Stream<CloudEvent> events) {
+
+  }
+}
